cd939c55ea85348c1bca4eadcb5f09cf92ce819c
joevansteen <joe.vansteen@gmail.com>
2023-05-31 18:12:11 -0700

    typo corrections

--- index.php
+++ index.php
@@ -13,13 +13,14 @@
 		</p>
 	<h2>Facilities</h2>
 		<ul>
-			<li><a href="\eats\src\EATSv5.pdf">Redirected Kinsta base code</a></li>
+			<li><a href="\eats\src\EATSv5.php">Redirected Kinsta base code</a></li>
+			<li><a href="\eats\src\simple.php">Redirected simple PHP code</a></li>
 			<li><a href="\phpGedView\">PHP GED View</a></li>
 			<li>Related materials</li>
 		</ul>
 	<h2>Related Sites</h2>
 		<ul>
-		<li><a href="https://sites.google.com/d/0BzkgAr42hG9nTGJLUTc5RGNXLWM/p/0BzkgAr42hG9nZ0FCMlBsZmE1Q1E">Architected Futures Portal</a></li>
+		<li><a href="https://sites.google.com/view/architectedfutures-portal/home">Architected Futures Portal</a></li>
 		<li><a href="https://architectedfutures.info">Architected Futures WordPress</a></li>
 		<li><a href="http://architectedfutures.net">Architected Futures Network - Drupal</a></li>
 		<li><a href="https://architectedfutures.net">Architected Futures Network - WP</a></li>

40c30ddc1d0fa6284335ed0532ac3ca6c5bcc1d9
joevansteen <joe.vansteen@gmail.com>
2023-07-20 16:12:10 -0700

    Prepare for v5.2023 beta

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
     <title>EATSv5 | Kinsta PHP PaaS Hosting</title>
     <meta charset="UTF-8">
     </head>	
-	<body><h1>Hi! Welcome to here!</h1>
+	<body><h1>Hi! Welcome to EATS!</h1>
+	<p>EATS is a proprietary product concieved and implemented by Architected Futures, LLC
+		 and Joe Van Steen, the original copyright holders.  This is official documentation
+		 for the official PHP implementation script for EATS. EATS is an intensional algorithm.
+		 In that form, EATS is an AI, an Algorithmic Intelligence which exemplifies and implements
+		 a tool suite of intelligence augmentation features.</p>
+	
 	<p>This is the current root directory for the EATSv5 Kinsta PHP PaaS hosted server.</p>
 	<p>In the prior implementation we used two distinct Apache servers to support
 		 Java Tomcat and PHP services.  The new server has module support for Tomcat,
@@ -27,19 +33,20 @@
 		</ul>
 	<h2>Related Sites</h2>
 		<ul>
-		<li><a href="https://sites.google.com/view/architectedfutures-portal/home">Architected Futures Portal</a></li>
-		<li><a href="https://architectedfutures.info">Architected Futures WordPress</a></li>
-		<li><a href="http://architectedfutures.net">Architected Futures Network - Drupal</a></li>
-		<li><a href="https://architectedfutures.net">Architected Futures Network - WP</a></li>
-		<li><a href="https://sites.google.com/view/architectedfutures">Architected Futures Google Main</a></li>
-		<li><a href="https://classroom.google.com/u/0/c/Njk4Njc2MTQwNVpa">Architected Futures Google Classroom</a></li>
-		<li><a href="https://architectedfutures.com">Architected Futures Network - COM</a></li>
-		<li><a href="https://architectedfutures.org">Architected Futures Network - ORG</a></li>
-		<li><a href="https://architectedfutures.us">Architected Futures WordPress - US</a></li>
-		<li><a href="https://architectedfutures.biz">Architected Futures Network - BIZ</a></li>
-		<li><a href="https://architectedfutures.exchange">Architected Futures WordPress - EXCHANGE</a></li>
-		<li><a href="https://148avenida.guru">148 Neighborhood</a></li>
-		<li><a href="https://jvs.guru/">JVS</a></li>
-		<li><a href="https://webtrees-c4w22.kinsta.app/index.php?route=%2Ftree%2Fjvstree0">Webtree</a></li>
+		<li><a href="http://localhost/AFV5.2023-jvs/" target="_blank">localhost</a></li>
+		<li><a href="https://sites.google.com/view/architectedfutures-portal/home" target="_blank">Architected Futures Portal</a></li>
+		<li><a href="https://architectedfutures.info" target="_blank">Architected Futures WordPress</a></li>
+		<li><a href="http://architectedfutures.net" target="_blank">Architected Futures Network - Drupal</a></li>
+		<li><a href="https://architectedfutures.net" target="_blank">Architected Futures Network - WP</a></li>
+		<li><a href="https://sites.google.com/view/architectedfutures" target="_blank">Architected Futures Google Main</a></li>
+		<li><a href="https://classroom.google.com/u/0/c/Njk4Njc2MTQwNVpa" target="_blank">Architected Futures Google Classroom</a></li>
+		<li><a href="https://architectedfutures.com" target="_blank">Architected Futures Network - COM</a></li>
+		<li><a href="https://architectedfutures.org" target="_blank">Architected Futures Network - ORG</a></li>
+		<li><a href="https://architectedfutures.us" target="_blank">Architected Futures WordPress - US</a></li>
+		<li><a href="https://architectedfutures.biz" target="_blank">Architected Futures Network - BIZ</a></li>
+		<li><a href="https://architectedfutures.exchange" target="_blank">Architected Futures WordPress - EXCHANGE</a></li>
+		<li><a href="https://148avenida.guru" target="_blank">148 Neighborhood</a></li>
+		<li><a href="https://jvs.guru/" target="_blank">JVS</a></li>
+		<li><a href="https://webtrees-c4w22.kinsta.app/index.php?route=%2Ftree%2Fjvstree0" target="_blank">Webtree</a></li>
 		</ul>
 	</body></html>
